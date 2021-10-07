374c615f294c7df767c5e780b43124f4850eecb7
Jarosław Kozioł <jaroslaw.koziol@inteliwise.com>
2021-10-07 15:57:30 +0200

    Modified

--- PractisePython/ex1.py
+++ PractisePython/ex1.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
-
 # Create a program that asks the user to enter their name and their age.
 # Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
 #Asks use to enter name
-name = input('What is your name? ')
+name = input("What is your name? ")
+print("Ok thanks sir, so your name is " + name)
 
 #Asks to enter their age
-age = input('What is your age? ')
+age = int(input("What is your age? "))
 
-#Print out a message
-print(name + 'In 100 years you will be ' + (age + 100) 'old')
+print("Your age is: ", age)
+
