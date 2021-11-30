1a7447e69da7419bfbd48c5a187a2b3f43a45e42
Jarosław Kozioł <jaroslaw.koziol@inteliwise.com>
2021-11-30 12:18:46 +0100

    Separated count value from json result

--- pajacyk.py
+++ pajacyk.py
@@ -7,4 +7,4 @@ url = 'http://pajacyk.pl//api/clicks'
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; XH; rv:8.578.498) fr, Gecko/20121021 Camino/8.723+ (Firefox compatible)'}
 
 response = requests.post(url, headers=headers)
-print(response.content)
+print(response.json()['count'])
